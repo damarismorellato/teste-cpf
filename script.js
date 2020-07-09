ba9fd81668c4bb330a6eb54b0dc663372500076a
damarismorellato <46945524+damarismorellato@users.noreply.github.com>
2020-07-09 14:35:45 -0300

    digito verificador

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 console.log('JavaScript carregado');
 
-function validaCPF(cpf){
+function validaCPF(cpf) {
+    console.log(cpf.length)
+    if (cpf.length != 11) {
     return false;
+    } else {
+
+        var numeros = cpf.substring(0, 9);
+
+        var digitos = cpf.substring(9);
+        
+        
+        var soma = 0;
+
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        console.log (soma);
+        return true;
+    }
 }
 
 //-- mostra que a validação foi iniciada
